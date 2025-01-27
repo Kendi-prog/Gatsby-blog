751b7289ab05eb8f2123aafcb5cc8fc25cb4b68f
Kendi <muriungikendileila@gmail.com>
2025-01-27 09:35:37 +0300

    adding the markdown pages

---
+++ src/pages/page-3.js
@@ -0,0 +1,15 @@
+import * as React from "react"
+
+
+import Layout from "../components/layout"
+
+
+const ThirdPage = () => (
+  <Layout>
+    <h1>Hello from page 3</h1>
+  </Layout>
+)
+
+
+
+export default ThirdPage
